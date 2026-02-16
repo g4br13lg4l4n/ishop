d00a341d7c1ddbcec8b98fb45dd3dfdd40e4d3e6
Gabriel Galan <gabriel.galan92@gmail.com>
2026-02-16 14:14:41 -0600

    Add database seeding and migration logic in Program.cs to ensure initial data setup

--- API/Program.cs
+++ API/Program.cs
@@ -31,4 +31,17 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+try {
+    // seed the database
+    // this is to ensure that the database is seeded with the data from the json file
+    using var scope = app.Services.CreateScope(); // this is to create a scope for the services
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync(); // this is to apply the migrations to the database
+    Console.WriteLine("Migrations applied");
+    await StoreContextSeed.SeedAsync(context); // this is to seed the database with the data from the json file
+} catch (Exception ex) {
+    Console.WriteLine(ex);
+}
+
 app.Run();
