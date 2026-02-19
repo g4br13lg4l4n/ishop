898f626925e6e1ecc2890448a27c52d8687439ac
Gabriel Galan <gabriel.galan92@gmail.com>
2026-02-18 20:32:04 -0600

    Enhance ProductSpecification and ProductSpecParams to support case-insensitive search functionality. Update Search property in ProductSpecParams to ensure consistent lowercase handling, improving query flexibility for product name and description filtering.

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -7,6 +7,9 @@ public class ProductSpecification : BaseSpecification<Product>
 {
     // this is to create the specification
     public ProductSpecification(ProductSpecParams specParams) : base(x =>
+    (string.IsNullOrEmpty(specParams.Search) || 
+    x.Name.ToLower().Contains(specParams.Search) || 
+    x.Description.ToLower().Contains(specParams.Search)) &&
     (specParams.Brands.Count == 0 || specParams.Brands.Contains(x.Brand)) &&
     (specParams.Types.Count == 0 || specParams.Types.Contains(x.Type))) // filter by brand and type
     {
